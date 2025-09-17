2462dde5781d6e97f32d8fe8648d633e604a9139
Маргарита Размахнина <ritysiav@gmail.com>
2025-09-17 09:06:35 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app. route("/")
+@app.route("/")
+@app.route("/web")
 def start():
     return """<!doctype html>
         <html>
-           <body>
-               <h1>web-сервер на flask</h1>
-           </body> 
+            <body>
+                <h1>web-сервер на flask</h1>
+                <a href="/author">Информация об авторе</a>
+            </body> 
         </html>""" 
+
+@app.route("/author")
+def author():
+    name = "Размахнина Маргарита Михайловна"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+    return f"""<!doctype html>
+        <html>
+            <body>
+                <p>Студент: {name}</p>
+                <p>Группа: {group}</p>
+                <p>Факультет: {faculty}</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
