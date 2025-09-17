d8e77b627aa72c3cb731a665ab33a44386b904b0
Маргарита Размахнина <ritysiav@gmail.com>
2025-09-17 10:17:51 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -26,3 +26,15 @@ def author():
                 <a href="/web">web</a>
             </body>
         </html>"""
+
+@app.route('/image')
+def image():
+    path = url_for("static", filename="ka.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Тимон</h1>
+        <img src="''' + path + '''">
+    </body>
+</html>'''
